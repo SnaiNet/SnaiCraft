00fec5085309e283a028cddb77de982a76cd59fd
snaiperskaya <aquarist1@gmail.com>
2021-12-23 16:31:51 -0500

    Snaicraft 1.3.4

--- kubejs/server_scripts/fixes.js
+++ kubejs/server_scripts/fixes.js
@@ -1,5 +1,6 @@
 onEvent('recipes', event => {
 
+	//Remove Quartz Glass smelting recipe for conflict with Silicon
 	event.remove({id: 'betternether:quartz_glass'})
 	
 	event.shaped('8x betternether:quartz_glass', [
@@ -10,5 +11,15 @@ onEvent('recipes', event => {
     S: '#forge:gems/quartz',
     A: '#forge:glass'
   })
+
+
+	//gold and iron chunks to vanilla ore
+	event.remove({id: 'emendatusenigmatica:ore_from_chunk_crafting/gold'})
+	event.remove({id: 'emendatusenigmatica:ore_from_chunk_crafting/iron'})
+	
+	event.shapeless('minecraft:gold_ore', ['#forge:ores/gold'])
+	event.shapeless('minecraft:iron_ore', ['#forge:ores/iron'])
+	
+
 
 })
