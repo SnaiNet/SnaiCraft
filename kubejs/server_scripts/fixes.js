10d64a2dd357228677b76f9a5b44b8dadfa620d0
snaiperskaya <aquarist1@gmail.com>
2021-12-24 09:33:05 -0500

    Update fixes.js

--- kubejs/server_scripts/fixes.js
+++ kubejs/server_scripts/fixes.js
@@ -20,6 +20,27 @@ onEvent('recipes', event => {
 	event.shapeless('minecraft:gold_ore', ['#forge:ores/gold'])
 	event.shapeless('minecraft:iron_ore', ['#forge:ores/iron'])
 	
-
+	
+	//Processing for iron, gold, and aluminum in mechanical squeezer
+	var ores = ['iron', 'gold', 'aluminum']
+	ores.forEach(function(ore) {
+		event.custom({
+			'type': 'integrateddynamics:mechanical_squeezer',
+			'item': `#forge:ores/${ore}`,
+			'result': {
+				'items': [
+					{
+						'item': `emendatusenigmatica:${ore}_dust`,
+						'count': 2
+					},
+					{
+						'item': `emendatusenigmatica:${ore}_dust`,
+						'chance': 0.5
+					}
+				]
+			},
+			'duration': 40
+		})
+	})
 
 })
